a2fee3d15ec41fd21ff1b4967037864b6ef0282c
angads554 <128231765+angads554@users.noreply.github.com>
2024-06-09 19:42:12 -0500

    Time: 0 ms (100%), Space: 45.6 MB (35.57%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int x=1;
+        int y=1;
+        for(int i=0;i<matrix.length;i++){
+            if(matrix[i][0]==0){
+                x=0;
+            }
+        }
+
+        for(int j=0;j<matrix[0].length;j++){
+            if(matrix[0][j]==0){
+                y=0;
+            }
+        }
+
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+
+        for(int i=1; i<matrix.length;i++){
+            if(matrix[i][0]==0){
+                for(int j=0;j<matrix[0].length;j++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        for(int j=1;j<matrix[0].length;j++){
+            if(matrix[0][j]==0){
+                for(int i=1;i<matrix.length;i++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        if(x==0){
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][0]=0;
+            }
+        }
+        if(y==0){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[0][j]=0;
+            }
+        }
+        
+    }
+}
