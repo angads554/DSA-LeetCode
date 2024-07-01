2234bc69955e662697cfa50c6e668b55c8289847
Angad Singh <128231765+angads554@users.noreply.github.com>
2024-07-01 12:14:59 -0500

    Time: 0 ms (100%), Space: 40.5 MB (26.43%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int [m][n];
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0) {
+                    dp[i][j]=1;
+                }else{
+                if(i==0&&j==0) dp[i][j]=0;
+                int up=0, down=0;
+                if(i>0) up= dp[i-1][j];
+                if(j>0) down= dp[i][j-1];
+                dp[i][j]= up+down;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
